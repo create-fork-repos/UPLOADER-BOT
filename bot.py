0572483f0eab205eb6b641b719fd5af4d121fb2b
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-05-04 19:43:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@
 # (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
 
 import os
-from config import Config
+
 from pyrogram import Client as Clinton
 
 if __name__ == "__main__" :
     # create download directory, if not exist
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
+    if not os.path.isdir(./DOWNLOADS):
+        os.makedirs(./DOWNLOADS)
     plugins = dict(root="plugins")
     Warrior = Clinton("@UPLOADER_X_BOT",
     bot_token="6186365167:AAF8ApYrxjvm-oR0I6CZE_OHyrki9a1RMVw",
