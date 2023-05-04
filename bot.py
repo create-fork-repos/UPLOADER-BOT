e49afebad2097b8cea99fafac8c764e7c48660cb
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-05-04 19:29:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ if __name__ == "__main__" :
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
     Warrior = Clinton("@UPLOADER_X_BOT",
-    bot_token=6186365167:AAF8ApYrxjvm-oR0I6CZE_OHyrki9a1RMVw,
+    bot_token="6186365167:AAF8ApYrxjvm-oR0I6CZE_OHyrki9a1RMVw",
     api_id=3704772,
     api_hash="b8e50a035abb851c0dd424e14cac4c06",
     plugins=plugins)
